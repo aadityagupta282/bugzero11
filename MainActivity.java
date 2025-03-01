0d060ae41f6130afdfbd1cda47d18592acc67df5
aadityagupta282 <collegeid5556@gmail.com>
2025-03-01 15:03:21 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.login_page;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2;
+    Button b1;
+    TextView t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        e1 = findViewById(R.id.editTextTextEmailAddress);
+        e2 = findViewById(R.id.editTextTextPassword);
+        b1 = findViewById(R.id.button);
+        t1 = findViewById(R.id.textView);
+
+        b1.s
+    }
+}
